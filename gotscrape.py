331347a8aab49215db985d8fb862e93da225ffa4
Chelsey <jacobs27c@gmail.com>
2017-08-30 11:55:02 +0100

    Complete name clean regex etc

--- gotscrape.py
+++ gotscrape.py
@@ -1,5 +1,6 @@
 import requests
 import pandas
+import re
 from bs4 import BeautifulSoup
 
 r = requests.get("https://en.wikipedia.org/wiki/List_of_A_Song_of_Ice_and_Fire_characters")
@@ -8,5 +9,17 @@ c = r.content
 soup = BeautifulSoup(c, "html.parser")
 
 all = soup.find_all("table", {"class": "navbox collapsible expanded"})[0]
-names = all.find("td").text.replace("[c]","").replace("[e]","").replace("[f]","").replace("[d]","").replace("[a]","").replace("[b]","").replace("\n",",")
-print(names.replace(",,", ""))
+rawnames = all.find("td").text.replace("\n\n","").replace("\n","").replace("  ","")
+cleannames = re.split(r"\[\w+\]", rawnames)
+namelist=[]
+for item in cleannames:
+    #I copied this regex from stackoverflow - it's putting a space between instances of lowercase
+    #followed by uppercase - correctly spacing the names
+    i = re.sub(r'([a-z])([A-Z])', r'\1 \2', item)
+    if i != str(""):
+        namelist.append(i)
+#names are cleaned via cleannames and appended to the namelist, which is a list of the data we need
+print(namelist)
+
+
+
