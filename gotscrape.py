0f5c069392e2fbe354ec061c4414af5d4833dc92
Chelsey <jacobs27c@gmail.com>
2017-09-03 16:44:10 +0100

    Finish URL checker & add to df/csv
    df/csv sorted so characters with dedicated pages are at the top of the list

--- gotscrape.py
+++ gotscrape.py
@@ -12,40 +12,58 @@ regex = ('(House|References|Secondary sources|Primary sources|Bibliography|Exter
 p = re.compile(regex)
 namelist=[]
 linklist = []
-
+checklist = []
 for item in all:
     name = item.text
     if p.match(name) is None:
-        namelist.append(name)
-        urlend = name.replace(" ", "_")
-        url = str("https://en.wikipedia.org/wiki/" + urlend)
-        linklist.append(url)
+        #gotta check for Ned specifically as his char page is Ned_Stark but name listed everywhere as Eddard
+        if name == "Eddard Stark":
+            namelist.append("Ned Stark")
+            urlend = "Ned_Stark"
+            url = str("https://en.wikipedia.org/wiki/" + urlend)
+            linklist.append(url)
+        else:
+            namelist.append(name)
+            urlend = name.replace(" ", "_")
+            url = str("https://en.wikipedia.org/wiki/" + urlend)
+            linklist.append(url)
     else:
         pass
 
+#character page exists checker - scrapes for redirect link text (this is the name of the character if redirected to
+# the 'list of characters' page
+a = 0
+for item in linklist:
+    request = requests.get(item)
+    z = request.content
+    soup2 = BeautifulSoup(z, "html.parser")
+    try:
+        redirected = soup2.find("a", {"class:", "mw-redirect"}).text
+        if redirected == namelist[a]:
+            # print("No")
+            checklist.append("No")
+        else:
+            # print("Yes")
+            checklist.append("Yes")
+    except:
+        # print("Fail")
+        checklist.append("No")
+    print(str(a) + " of 124 entries checked")
+    a = a + 1
+'''
+Dataframe issue?
+Look at this awesome page:
+
+http://queirozf.com/entries/pandas-dataframe-by-example
+'''
+
 df = pandas.DataFrame(
     {'Name': namelist,
-     'URL': linklist})
-df.to_csv("data.csv")
-
-#this is a WIP, trying to filter out only the URLs which lead to actual character pages
-# a = 0
-# for b in linklist[a]:
-#     # print(linklist[a])
-#     request = requests.get(linklist[a])
-#     # print(request.headers)
-#     print(request.status_code)
-#     print(request.url)
-#     if request.status_code == 404:
-#         print("None")
-#     elif request.status_code == 304:
-#         print("None")
-#     else:
-#         print("Ok")
-#     a=a+1
-
-# print(namelist)
-# print(linklist)
+     'URL': linklist,
+     'Page': checklist})
+#this line sorts the rows with dedicated pages to the top of the list for testing
+sorted_df = df.sort_values('Page',ascending=False)
+sorted_df.to_csv("data.csv")
 
 
 
